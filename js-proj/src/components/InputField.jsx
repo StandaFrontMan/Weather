a8651c5a1c2718698bada9084fc0e1dfdcc5c55b
Standa Vin <vinninjaq@gmail.com>
2023-06-26 01:10:37 +0200

    Styled InputField

--- js-proj/src/components/InputField.jsx
+++ js-proj/src/components/InputField.jsx
@@ -5,12 +5,15 @@ const InputField = ({ cityName, setCityName, handleSearch }) => {
     <div className='input-area'>
         <div className='input_area'>
         <input
+          className='main-input'
           type='text'
           placeholder='Enter City name'
           value={cityName}
           onChange={(e) => setCityName(e.target.value)}
         />
-        <button onClick={handleSearch}>Search</button>
+        <div className='button-div'>
+          <button className='button' onClick={handleSearch}>Find out the weather ... </button>
+        </div>
       </div>
     </div>
   )
