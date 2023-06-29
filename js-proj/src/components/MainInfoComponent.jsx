a6791877ba78837281ffbaf3880eceab4ec960f5
Standa Vin <vinninjaq@gmail.com>
2023-06-29 20:51:45 +0200

    Styled MainInfoComponent

--- js-proj/src/components/MainInfoComponent.jsx
+++ js-proj/src/components/MainInfoComponent.jsx
@@ -9,12 +9,14 @@ const MainInfoComponent = () => {
     <div className='main-info-block'>
         {status === 'resolved' && city && city.main && city.weather && (  
           <div className='info'>
-            <h2>Current Weather in {city.name}</h2>
+            <h2 className='info-header'>Current Weather in {city.name}</h2>
             <div className='forecast-container'>
               <div className='temp-container'>
                 <div className='curr-temp'>{Math.round(city.main.temp)}°C</div>
-                <div className='feels-like-temp'>Feels like {Math.round(city.main.feels_like)}°C</div>
-                <div className='temp-discription'>{city.weather[0].main}</div>
+                  <div className='minor-temp-info'>
+                    <div className='feels-like-temp'>Feels like {Math.round(city.main.feels_like)}°C</div>
+                    <div className='temp-discription'>{city.weather[0].main}</div>
+                  </div>
               </div>
             </div>
           </div>
